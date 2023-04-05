9e9dfad27ac283baed55fc0ac5b4b18cac33ba24
donghanyuan <dhy_120@163.com>
2023-04-06 02:58:09 +0800

    fix docker run example

--- docker-run.example.sh
+++ docker-run.example.sh
@@ -5,6 +5,6 @@ PATH_TO_DATA=
 
 docker run -d --restart=unless-stopped --name="birthday-remainder" \
     -v /etc/timezone:/etc/timezone:ro -v /etc/localtime:/etc/localtime:ro \
-    -v ${PATH_TO_DATA} /app/data.xlsx \
-    -v ${PATH_TO_KEY} /app/key \
+    -v ${PATH_TO_DATA}:/app/data.xlsx:ro \
+    -v ${PATH_TO_KEY}:/app/key:ro \
     birthday-remainder:latest
